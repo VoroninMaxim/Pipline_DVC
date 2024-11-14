ca9aaab8f1cc57d01c22ba312c5f0414cfac0ea0
VoroninMaxim <153994770+VoroninMaxim@users.noreply.github.com>
2024-11-15 02:51:36 +0300

    Update decrypt_secret.sh

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-# Decrypt the file
+chmod +x ./decrypt_secret.sh 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output ./.dvc/service_account.json ./.dvc/service_account.json.gpg
+
+# Check if decryption was successful
+if [ $? -eq 0 ]; then
+    echo "Decryption successful!"
+else
+    echo "Decryption failed. Please check if the correct keys are available and if the file is valid."
+    exit 1
+fi
