e149d6eb0273096188f4b733302783224520f534
VoroninMaxim <153994770+VoroninMaxim@users.noreply.github.com>
2024-11-15 01:46:11 +0300

    Update decrypt_secret.sh

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -5,4 +5,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/.dvc/service_account.json.gpg
+--output ./.dvc/service_account.json.gpg
